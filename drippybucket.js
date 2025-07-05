2b2c370e389be5849ae777b19137817c7222404f
james-ssh <james0ssh@gmail.com>
2025-07-05 15:00:15 +1000

    Create DrippyBucket.js

---
+++ drippybucket.js
@@ -0,0 +1,96 @@
+class DrippyBucket {
+  #queue = [];
+  #rate;
+  #limit = undefined;
+  #lastFetched_mil = 0;
+
+  /**
+   * A leaky bucket that does rates of s^-1, numbers over 1 will be considered per second and numbers below 1 will be considered in milliseconds
+   * @param {Number} rate
+   * @returns DrippyBucket
+   */
+  #setRateLimit(rate) {
+    if (rate < 1) {
+      this.rate = rate * 1000;
+      return this;
+    }
+
+    this.rate = (1 / rate) * 1000;
+    if (typeof this.rate !== Number) {
+      console.warn("Rate not set");
+      this.rate = 0;
+    }
+
+    return this;
+  }
+
+  constructor(rate, limit) {
+    this.#setRateLimit(rate);
+    if (typeof limit === Number && limit > -1) {
+      this.#limit = limit;
+    } else {
+      console.warn("Limit not set");
+    }
+
+    return this;
+  }
+
+  add(obj) {
+    if (this.#checkokayinsert()) {
+      this.#queue.push(obj);
+      return true;
+    }
+    return false;
+  }
+
+  insert(obj) {
+    return this.add(obj);
+  }
+
+  push(obj) {
+    return this.add(obj);
+  }
+
+  pushBack(obj) {
+    return this.add(obj);
+  }
+
+  insertBlocking(obj) {
+    while (this.add(obj) == false) {}
+  }
+
+  async insertAsync(obj) {
+    while (this.add(obj) == false) {}
+  }
+
+  /**
+   * Checks if able to insert
+   * @returns Boolean
+   */
+  #checkokayinsert() {
+    if (this.#limit < this.#queue.length || this.#limit === undefined) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  #get() {
+    return this.#queue.pop_front();
+  }
+
+  getBlocking() {
+    let obj = undefined;
+    while (Date.now() < this.#lastFetched_mil + this.#rate) {}
+
+    while (obj === undefined) {
+      obj = this.#get();
+    }
+
+    return obj;
+  }
+
+  async getAsync() {
+    return this.getBlocking();
+  }
+}
